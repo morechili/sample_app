eca66f9d0b856ce3ee20c9f60dad1baae2e54fe0
Paul <github@pauldinin.com>
2012-01-27 18:16:05 -0700

    Finished first half of User Model

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.0'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.8.1'
+  gem 'annotate'
 end
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 SampleApp::Application.routes.draw do
   
-  get "users/new"
-
+  resources :users
+  match '/signup',  :to => 'users#new'
+  
   match '/contact', :to => 'static_pages#contact'
   match '/about',   :to => 'static_pages#about'
   match '/help',    :to => 'static_pages#help'
-  match '/signup',  :to => 'users#new'
+
   
   root :to => 'static_pages#home'
   
